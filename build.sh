dedee975a9f961bf8e9ae39c2129d50680c1cbe2
zedaes <147769903+zedaes@users.noreply.github.com>
2025-10-02 22:46:56 -0400

    update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-
 #!/bin/bash
 
+SIZE=${2:-9}
+
 if [ -z "$1" ]; then
-    echo "Usage: ./build.sh <number_of_puzzles>"
+    echo "Usage: ./build.sh <number_of_puzzles> [sudoku_size]"
     exit 1
 fi
 
-gcc generate.c -o generate -O2
+gcc -o generate -O2 -DSIZE=$SIZE generate.c solve.c -lm
 
-./generate $1
+./generate "$1"
